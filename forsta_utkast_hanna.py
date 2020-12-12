4307a84c77d5b41aa4b317c763c616be75a28361
hannaisr <hannaisr@kth.se>
2020-12-12 21:41:58 +0100

    Change so that the visited positions are atored in a way that makes self avoiding walk easier to implement

--- forsta_utkast_hanna.py
+++ forsta_utkast_hanna.py
@@ -4,17 +4,13 @@ import matplotlib.pyplot as plt # For 3D plot
 from mpl_toolkits.mplot3d import Axes3D # For 3D plot
 
 class Walker():
-    """Walked positions are stored in three lists, one for each coordinate."""
-    # Initial position
-    x0 = 0
-    y0 = 0
-    z0 = 0
+    """Walked positions are stored in a list"""
+    # Initiate list of visited points
+    origin = [0,0,0]
+    visited_points = [origin]
 
     # Define step length
     r = 1
-
-    # Initiate the list storing the coordinates of the walk
-    x, y, z = [x0], [y0], [z0]
 
     def walk_one_step(self):
         """Implemented by child class"""
@@ -30,19 +26,23 @@ class Walker():
         pass
 
     def restart(self):
-        """Resets the coordinate lists."""
-        self.x, self.y, self.z = [self.x0], [self.y0], [self.z0]
+        """Resets list of visited points."""
+        visited_points = [[0,0,0]]
 
     def plot_the_walk(self):
         """Plots the walk in 3D."""
+        x = [i[0] for i in self.visited_points]
+        y = [i[1] for i in self.visited_points]
+        z = [i[2] for i in self.visited_points]
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.plot(self.x,self.y,self.z)
+        ax.plot(x,y,z)
         plt.show()
 
     def get_end_to_end_distance(self):
         """Calculate end-to-end distance of already walked walk."""
-        return np.sqrt((self.x[-1]-self.x0)**2+(self.y[-1]-self.y0)**2+(self.z[-1]-self.z0)**2)
+        last_point = self.visited_points[-1]
+        return np.sqrt((last_point[0]-self.origin[0])**2+(last_point[1]-self.origin[1])**2+(last_point[2]-self.origin[2])**2)
 
     def get_multiple_end_to_end_distances(self,nwalks=10):
         """Returns a list of end-to-end distances for nwalks number of walks"""
@@ -56,51 +56,57 @@ class Walker():
 
 class Grid_walker(Walker):
     def walk_one_step(self):
-        # Append the same coordinates as last step to coordinate list
-        self.x.append(self.x[-1]), self.y.append(self.y[-1]), self.z.append(self.z[-1])
+        current_pos = self.visited_points[-1][:]
         # Get walking direction
         direction = rnd.randint(0,5)
         # Update the coordinates
         if direction == 0:
-            self.x[-1] += 1
+            current_pos[0] += 1
         elif direction == 1:
-            self.x[-1] -= 1
+            current_pos[0] -= 1
         elif direction == 2:
-            self.y[-1] += 1
+            current_pos[1] += 1
         elif direction == 3:
-            self.y[-1] -= 1
+            current_pos[1] -= 1
         elif direction == 4:
-            self.z[-1] += 1
+            current_pos[2] += 1
         elif direction == 5:
-            self.z[-1] -= 1
+            current_pos[2] -= 1
+        # Update list of visited points
+        self.visited_points.append(current_pos)
 
     def walk_with_self_avoid(self,nsteps=100):
         """Walk nsteps steps of self-avoiding random walk"""
-        pass
+        for i in range(nsteps):
+            walk_one_step()
+            if self.visited_points[-1] in self.visited_points[:-1]:
+                print('Managed to walk',len(visited_points)-1,'steps')
+                break
 
 class Freely_jointed_chain(Walker):
     # Define radius of spheres at ends for self-avoiding walk (could perhaps be put in __init__())
     R = 1/2
 
     def walk_one_step(self):
-        # Append the same coordinates as last step to coordinate list
-        self.x.append(self.x[-1]), self.y.append(self.y[-1]), self.z.append(self.z[-1])
+        current_pos = self.visited_points[-1]
         # Get walking direction
         theta = rnd.uniform(0,np.pi)
         phi = rnd.uniform(0,2*np.pi)
         # Update the coordinates
-        self.x[-1] += self.r*np.sin(theta)*np.cos(phi)
-        self.y[-1] += self.r*np.sin(theta)*np.sin(phi)
-        self.z[-1] += self.r*np.cos(theta)
+        current_pos[0] += self.r*np.sin(theta)*np.cos(phi)
+        current_pos[1] += self.r*np.sin(theta)*np.sin(phi)
+        current_pos[2] += self.r*np.cos(theta)
+        # Update list of visited points
+        self.visited_points.append(current_pos)
 
     def walk_with_self_avoid(self,nsteps=100):
         """Walk nsteps steps of self-avoiding random walk"""
         pass
 
 gridwalk = Grid_walker()
-print(gridwalk.get_multiple_end_to_end_distances(nwalks=10))
-# gridwalk.walk_without_avoid(nsteps=500)
-# gridwalk.plot_the_walk()
+# print(gridwalk.get_multiple_end_to_end_distances(nwalks=10))
+gridwalk.walk_without_avoid(nsteps=500)
+gridwalk.plot_the_walk()
 
 # chainwalk = Freely_jointed_chain()
 # chainwalk.walk_without_avoid(nsteps=500)
